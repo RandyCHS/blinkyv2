1a3de0433689776a789aa0ff08b44736085298a9
Randy Macdonald <41399033+RandyCHS@users.noreply.github.com>
2023-05-23 21:08:07 -0700

    Added buttonA and B presses

--- main.ts
+++ main.ts
@@ -1,2 +1,10 @@
+input.buttonA.onEvent(ButtonEvent.Click, function () {
+    light.showAnimation(light.rainbowAnimation, 1000)
+    light.setAll(0x000000)
+})
+input.buttonB.onEvent(ButtonEvent.Click, function () {
+    light.showAnimation(light.theaterChaseAnimation, 1000)
+    light.setAll(0x000000)
+})
 light.showAnimation(light.rainbowAnimation, 500)
 light.setAll(0x000000)
